03beb3910487efbe8f9cb314edcb5063ee9239c2
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-04-12 20:38:57 -0300

    feat: updated attributes and removed flagRepository.js file

--- src/repositories/userRepository.js
+++ src/repositories/userRepository.js
@@ -4,20 +4,42 @@ const Users = require("../models/userModel");
 const Repository = require("./repository");
 
 class UserRepository extends Repository {
-  constructor() {
-    super(Users);
-  }
+	constructor() {
+		super(Users);
+	}
 
-  getAll = async () => {
-    return await this.model.findAll({ attributes: ["nickname", "email", "createdAt"] });
-  };
+	getAll = async () => {
+		return await this.model.findAll({
+			attributes: [
+				"nickname",
+				"email",
+				"cep",
+				"street",
+				"neighborhood",
+				"city",
+				"state",
+				"createdAt",
+			],
+		});
+	};
 
-  getByEmail = async (email) => {
-    return await this.model.findOne({
-      where: { email },
-      attributes: ["id", "nickname", "email", "password", "createdAt"],
-    });
-  };
+	getByEmail = async (email) => {
+		return await this.model.findOne({
+			where: { email },
+			attributes: [
+				"id",
+				"nickname",
+				"email",
+				"password",
+				"cep",
+				"street",
+				"neighborhood",
+				"city",
+				"state",
+				"createdAt",
+			],
+		});
+	};
 }
 
 module.exports = new UserRepository();
