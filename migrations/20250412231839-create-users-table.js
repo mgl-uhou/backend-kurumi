f19bb89a29824e52ca089f5bcc72734f0bfe7ec7
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-04-12 20:33:13 -0300

    feat: created new migration for  kurumi's business objects

---
+++ migrations/20250412231839-create-users-table.js
@@ -0,0 +1,59 @@
+'use strict';
+
+module.exports = {
+  async up(queryInterface, Sequelize) {
+    await queryInterface.createTable('Users', {
+      id: {
+        allowNull: false,
+        autoIncrement: true,
+        primaryKey: true,
+        type: Sequelize.INTEGER
+      },
+      nickname: {
+        type: Sequelize.STRING(30),
+        allowNull: false
+      },
+      email: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        unique: true
+      },
+      password: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      cep: {
+        type: Sequelize.STRING(9),
+        allowNull: false
+      },
+      street: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      neighborhood: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      city: {
+        type: Sequelize.STRING,
+        allowNull: false
+      },
+      state: {
+        type: Sequelize.STRING(2),
+        allowNull: false
+      },
+      createdAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      },
+      updatedAt: {
+        allowNull: false,
+        type: Sequelize.DATE
+      }
+    });
+  },
+
+  async down(queryInterface, Sequelize) {
+    await queryInterface.dropTable('Users');
+  }
+};
