20461d1cd978bb5af45a3db3b234a9bcbaa6b00a
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-04-12 20:35:59 -0300

    feat: updated conection for SQLite

--- src/database/dbConnection.js
+++ src/database/dbConnection.js
@@ -4,12 +4,9 @@ const { Sequelize } = require("sequelize");
 require("dotenv").config();
 
 const sequelize = new Sequelize(
-  process.env.DB_NAME || "",   
-  process.env.DB_USER || "",    
-  process.env.DB_PASS || "",    
   {
-    host: process.env.DB_HOST || "",
-    dialect: process.env.DB_DIALECT || "postgres",
+    dialect: process.env.DB_DIALECT || "sqlite",
+    storage: process.env.DB_STORAGE || "./database.sqlite",
     logging: false, // Desabilita logs de queries
   }
 );
