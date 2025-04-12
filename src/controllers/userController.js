4d08fdce4257c1e6f37683d3ad2325aa630e610e
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-04-12 20:35:16 -0300

    feat: updated userController for new attributes

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -8,40 +8,50 @@ const errorFunction = require("../validators/errorFunction");
 require("dotenv").config();
 
 class UserController {
-	async post(req, res){
-		const { nickname, email, password } = req.body;
-		const validationContract = new ValidationContract();
+    async post(req, res){
+        const { nickname, email, password, cep, street, neighborhood, city, state } = req.body;
+        const validationContract = new ValidationContract();
 
-		validationContract.hasMinLen(nickname, 3, "O apelido deve conter pelo menos 3 caracteres.");
-		validationContract.hasMaxLen(nickname, 30, "O apelido deve conter no máximo 30 caracteres.");
-		validationContract.haveSpaces(nickname, "O apelido não pode conter espaços.");
-		validationContract.isEmail(email);
-		validationContract.isPassword(password);
+        validationContract.hasMinLen(nickname, 3, "O apelido deve conter pelo menos 3 caracteres.");
+        validationContract.hasMaxLen(nickname, 30, "O apelido deve conter no máximo 30 caracteres.");
+        validationContract.haveSpaces(nickname, "O apelido não pode conter espaços.");
+        validationContract.isEmail(email);
+        validationContract.isPassword(password);
+        validationContract.isCEP(cep, "CEP inválido.");
+        validationContract.isRequired(street, "Rua é obrigatória.");
+        validationContract.isRequired(neighborhood, "Bairro é obrigatório.");
+        validationContract.isRequired(city, "Cidade é obrigatória.");
+        validationContract.isState(state, "Estado inválido.");
 
-		if(!validationContract.isValid()){
-			res.status(400).json(validationContract.getErrors());
-			return;
-		}
-		
-		try {
-			const userExists = await userRepository.getByEmail(email);
-			if(userExists) throw new Error("E-mail já cadastrado.");
+        if(!validationContract.isValid()){
+            res.status(400).json(validationContract.getErrors());
+            return;
+        }
+        
+        try {
+            const userExists = await userRepository.getByEmail(email);
+            if(userExists) throw new Error("E-mail já cadastrado.");
 
-			const hashPassword = await bcrypt.hash(password + process.env.SALT, 10);
-			const user = await userRepository.create({
-				nickname,
-				email,
-				password: hashPassword
-			})
-			const { password: _, __v, ...userData } = user.toJSON();
-			res.status(201).json({
-				message: "Usuário cadastrado com sucesso.",
-				user: userData
-			});
-		} catch (e) {
-			errorFunction(res, e.message, "Falha ao cadastrar usuário.", 400);
-		}
-	}
+            const hashPassword = await bcrypt.hash(password + process.env.SALT, 10);
+            const user = await userRepository.create({
+                nickname,
+                email,
+                password: hashPassword,
+                cep,
+                street,
+                neighborhood,
+                city,
+                state: state.toUpperCase()
+            })
+            const { password: _, ...userData } = user.toJSON();
+            res.status(201).json({
+                message: "Usuário cadastrado com sucesso.",
+                user: userData
+            });
+        } catch (e) {
+            errorFunction(res, e.message, "Falha ao cadastrar usuário.", 400);
+        }
+    }
 
 	async login(req, res) {
 		const { email, password } = req.body;
@@ -67,33 +77,38 @@ class UserController {
 
 	getProfile = async (req, res) => res.status(200).json(req.user);
 
-	async put(req, res){
-		const data = req.body;
-		delete data.createdAt;
-		delete data.id;
+    async put(req, res){
+        const data = req.body;
+        delete data.createdAt;
+        delete data.id;
 
-		const validationContract = new ValidationContract();
+        const validationContract = new ValidationContract();
 
-		if(data.nickname) validationContract.hasMinLen(data.nickname, 3, "O apelido deve conter pelo menos 3 caracteres.");
-		if(data.nickname) validationContract.hasMaxLen(data.nickname, 30, "O apelido deve conter no máximo 30 caracteres.");
-		if(data.nickname) validationContract.haveSpaces(data.nickname, "O apelido não pode conter espaços.");
-		if(data.email) validationContract.isEmail(data.email);
-		if(data.password) validationContract.isPassword(data.password);
+        if(data.nickname) {
+            validationContract.hasMinLen(data.nickname, 3, "O apelido deve conter pelo menos 3 caracteres.");
+            validationContract.hasMaxLen(data.nickname, 30, "O apelido deve conter no máximo 30 caracteres.");
+            validationContract.haveSpaces(data.nickname, "O apelido não pode conter espaços.");
+        }
+        if(data.email) validationContract.isEmail(data.email);
+        if(data.password) validationContract.isPassword(data.password);
+        if(data.cep) validationContract.isCEP(data.cep, "CEP inválido.");
+        if(data.state) validationContract.isState(data.state, "Estado inválido.");
 
-		if(!validationContract.isValid()){
-			res.status(400).json(validationContract.getErrors());
-			return;
-		}
+        if(!validationContract.isValid()){
+            res.status(400).json(validationContract.getErrors());
+            return;
+        }
 
-		try {
-			if(data.password) data.password = await bcrypt.hash(data.password + process.env.SALT, 10);
-			
-			await userRepository.update(req.user.id, data);
-			res.status(200).json({ message: "Usuário atualizado com sucesso."});	
-		} catch (error) {
-			errorFunction(res, error.message, "Falha ao atualizar o usuário.", 400);
-		}
-	}
+        try {
+            if(data.password) data.password = await bcrypt.hash(data.password + process.env.SALT, 10);
+            if(data.state) data.state = data.state.toUpperCase();
+            
+            await userRepository.update(req.user.id, data);
+            res.status(200).json({ message: "Usuário atualizado com sucesso."});	
+        } catch (error) {
+            errorFunction(res, error.message, "Falha ao atualizar o usuário.", 400);
+        }
+    }
 
 	async delete (req, res){
 		try {
