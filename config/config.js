e73bba6b08993bbc319446a5c2ee19abf2a7b8b0
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-04-12 20:31:07 -0300

    feat: updated config for kurumi's database

--- config/config.js
+++ config/config.js
@@ -3,25 +3,9 @@
 require('dotenv').config();
 
 module.exports = {
-  development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-  },
-  test: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
-  },
-  production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    dialect: 'postgres',
+  development: { 
+    dialect: "sqlite",
+    storage: "./database.sqlite",
+    logging: false,
   },
 };
