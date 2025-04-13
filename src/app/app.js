9abd3c9e45a6e073708be0df6ab495daec50215f
mgl-uhou <miguelferreiralaurentino@gmail.com>
2025-04-13 00:16:46 -0300

    feat: cors updated

--- src/app/app.js
+++ src/app/app.js
@@ -1,5 +1,3 @@
-"use strict";
-
 const express = require("express");
 const cors = require("cors");
 const path = require("path"); 
@@ -9,10 +7,14 @@ const usersRoute = require("../routes/usersRoute");
 
 const app = express();
 
+// Configuração do CORS
+app.use(cors({
+    origin: '*',
+    methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS']
+}));
+
 app.use(express.json());
 app.use("/api-docs", swaggerUi.serve, swaggerUi.setup(swaggerDocs));
-
-app.use(cors());
 
 app.use("/users", usersRoute);
 app.use(express.static(path.join(__dirname, "../../../public")));
